aafec02c116792e6a78a18d0d5fea1d7b815c9c3
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-07-27 19:38:36 +0200

    initial commit with some setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem eum cum animi, sed enim voluptatem rerum, tempora magni praesentium error maiores qui architecto officiis unde possimus tempore ullam labore saepe.</p>;
 }
 
 export default App;
