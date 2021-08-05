b2526c02b582594087292102434e30eac2df1630
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-08-05 17:27:42 +0200

    made a beginning to the footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Grid from "./Grid";
 import './css/App.css'; 
 import DetailPage from "./DetailPage";
-
+import Footer from "./Footer";
 const App = () => {
     return ( 
         <div>
             <DetailPage slideshow="stop slideshow"></DetailPage>
+            <Footer></Footer>
         </div>
     );
 }
---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import "./css/Footer.css";
+
+const Footer = props => {
+    return (
+        <footer className="footer">
+            <header className="footer__headers">
+                <h4 class="footer__paintingHeader">Starry Night</h4>
+                <h5 class="footer__artistHeader">Vincent van Gogh</h5>
+            </header>
+            <section className="footer__buttons">
+            <h4 class="footer__paintingHeader">Starry Night</h4>
+                <h5 class="footer__artistHeader">Vincent van Gogh</h5>
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
