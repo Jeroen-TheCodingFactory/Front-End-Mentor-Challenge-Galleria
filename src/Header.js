54d6d5de5e0894c18eefe58789c1c1642ce4b57d
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-08-07 19:25:00 +0200

    fixed header

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,9 @@ import "./css/Header.css";
 const Header = ({slideshow, link}) => {
     return ( 
         <header class="header">
-            <Link class="header__start_slideshow" to={link}>
+            <Link class="header__start_slideshow" to="/">
                 <figure className="header__logo">
-                <img src="/img/shared/logo.svg" alt="galleria site logo" />
+                    <img src="/img/shared/logo.svg" alt="galleria site logo" />
                 </figure>
             </Link>
            <Link class="header__start_slideshow" to={link}>{slideshow}</Link>
