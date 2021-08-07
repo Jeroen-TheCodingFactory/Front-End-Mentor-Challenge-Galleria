9f5d5bb4a9248e4f8f175aee1d40eab7e48e7f87
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2021-08-07 19:35:42 +0200

    made data sections

---
+++ src/data/paintings.js
@@ -0,0 +1,78 @@
+export const data = [
+    {
+        id: 1,
+        title: "The Starry Night",
+        author: "Vincent van Gogh"
+    },
+    {
+        id: 2,
+        title: "Girl with a Pearl Earring",
+        author: "Johannes Vermeer",
+    },
+    {
+        id: 3,
+        title: "Guernica",
+        author: "Pablo Picasso",
+    },
+    {
+        id: 4,
+        title: "Pernitent Magdalene",
+        author: "Artemisia Gentileschi",
+    },
+    {
+        id: 5,
+        title: "The Storm on the Sea of Galilee",
+        author:"Rembrandt",
+    },
+    {
+        id: 6,
+        title:"The Great Wave off Kanagawa",
+        author: "Hokusai",
+    },
+    {
+        id: 7,
+        title: "Van Gogh self-portrait",
+        author: "Vincent van Gogh"
+    },
+    {
+        id: 8,
+        title: "The Sleeping Gypsy",
+        author: "Henri Rousseau",
+    },
+    {
+        id: 9,
+        title: "Lady with an Ermin",
+        author: "Leonardo da Vinci",
+    },
+    {
+        id:10,
+        title: "The Night Café",
+        author: "Vincent van Gogh",
+    },
+    {
+        id: 11,
+        title: "The Basket of Apples",
+        author: "Paul Cezanne",
+    },
+    {
+        id: 12,
+        title: "The Boy in the Red Vest",
+        author: "Edward Hopper",
+    },
+    {
+        id: 13,
+        title: "Arnolfini Portrait",
+        author: "Jan van Eyck",
+    },
+    {
+        id: 14,
+        title:"Mona Lisa",
+        author: "Leonardo da Vinci",
+    },
+    {
+        id: 15,
+        title: "The Swing",
+        author: "Jean-Honore Fragonard",
+    },
+
+];
